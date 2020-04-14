063cf44ebf8259cf2531fee2a3a0f80d7284b1cb
Jake Howlett <41288146+jakehowlett93@users.noreply.github.com>
2020-04-14 09:27:38 +0100

    added dlc and player scores

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 $(document).ready(function () {
+
+    const showScoreForm = () => {
+        let selectedElementsList = $('.selected')
+        console.log(selectedElementsList)
+        if  (!selectedElementsList.length % 2 === 0) {
+            $('.form:first').removeClass('hideElement')
+        }
+    }
+
+    const handleSelectClick = (event) => {
+        const element = $(event.target)
+        if (element.hasClass("selected")) {
+            element.removeClass('selected')
+        } else {
+            element.addClass("selected");
+        }
+    }
     
-    const table = $('<table></table>').click((event) => {handleClick(event)});
+    const table = $('<table></table>');
+    table.addClass('table')
     let counter = 1
-    for (let column = 0; column < 6; column++) {
+    for (let row = 0; row < 7; row++) {
         let tableRow = $('<tr></tr>');
-        for (let row = 0; row < 13; row++) {
-            if (counter < 75) {
-                var rowData = $('<td></td>');
-                rowData.append($('<img>',{id:counter, src:'./portraits/'+counter+'.png', class:'square'}))
+        for (let column = 0; column < 13; column++) {
+            if (counter < 81) {
+                const rowData = $('<td></td>');
+                let portrait = $('<img>',{id:counter, src:'./portraits/'+counter+'.png', class:'square'})
+                rowData.append(portrait)
+                rowData.click((event) => {handleSelectClick(event)})
                 tableRow.append(rowData);
                 counter++
             }
@@ -15,13 +35,30 @@ $(document).ready(function () {
         table.append(tableRow);
     }
     $(".main").append(table);
+    $('.table').insertBefore($('.scores:first'))
+    showScoreForm()
+
+    let playerOneScore = 0
+    let playerTwoScore = 0
+    const playerOne = $('.playerOneScore:first')
+    playerOne.append(playerOneScore)
+    const playerTwo = $('.playerTwoScore:first')
+    playerTwo.append(playerTwoScore)
+
+    const playerOneButton = $('.playerOneButton:first')
+    playerOneButton.click(() => {handleScoreClick1(playerOne)})
+    const playerTwoButton = $('.playerTwoButton:first')
+    playerTwoButton.click(() => {handleScoreClick2(playerTwo)})
     
-    const handleClick = (event) => {
-        const element = $(event.target)
-        if (element.hasClass("selected")) {
-            element.removeClass('selected')
-        } else {
-            element.addClass("selected");
-        }
+    //will refactor these into one function eventually
+    const handleScoreClick1 = (player) => {
+        player.empty()
+        playerOneScore++
+        player.append(playerOneScore)
+    }
+    const handleScoreClick2 = (player) => {
+        player.empty()
+        playerTwoScore++
+        player.append(playerTwoScore)
     }
 });
