7d6e25ae5908f3fededf7f3b508fe25798cd4839
Jake Howlett <41288146+jakehowlett93@users.noreply.github.com>
2020-04-15 21:10:07 +0100

    went in for a quick edit but endit up changing quite a bit
    improved styling
    added color shading to match player who selected portrait
    added custom cursors

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
+    const totalCharacters = 80
+
     const showScoreForm = (selectedElements) => {
         if  (selectedElements.length % 2 === 0 && selectedElements.length > 0) {
             $('.form').removeClass('hideElement')
@@ -10,49 +12,71 @@ $(document).ready(function () {
 
     const handleSelectClick = (event, selectedElements) => {
         const element = $(event.target)
-        if (element.hasClass("selected")) {
-            element.removeClass('selected')
-            selectedElements.pop()
-        } else {
+        
+        
+        if (!element.hasClass('selected')) {
+            if (selectedElements.length % 2 === 0) {
+                element.parent().addClass('redTint')
+                $(portraits).removeClass('playerOneCursor')
+                $(portraits).addClass('playerTwoCursor')
+            } else {
+                element.parent().addClass('blueTint')
+                $(portraits).removeClass('playerTwoCursor')
+                $(portraits).addClass('playerOneCursor')
+            }
             element.addClass("selected");
             selectedElements.push(1)
-            showScoreForm(selectedElements)
+            showScoreForm(selectedElements)          
         }
+
     }
 
-    const handleScoreClick = (playerDiv, playerScore) => {
-        playerDiv.empty()
-        playerDiv.append(playerScore)
+    const handleScoreClick = (playerElement, playerScore, player) => {
+        playerElement.empty()
+        playerElement.append(playerScore)
         $('.form').addClass('hideElement')
+        const scorePlayerWinsAt =  (totalCharacters / 2)
+        if (playerScore === scorePlayerWinsAt - 5) {
+            alert(player + ' is 5 wins from winning the smash down')
+        }
+        else if (playerScore === scorePlayerWinsAt - 1) {
+            alert(player + ' is 1 win from winning the smash down')
+        }
+        else if (playerScore > scorePlayerWinsAt) {
+            alert(player + ' has won!')
+        }
     }
     let selectedElements = [];
     
     const portraits = $('.portraits:first');
-    portraits.addClass('table')
-    for (let element = 1; element < 81; element++) {
+    portraits.addClass('table playerOneCursor')
+    for (let element = 1; element < totalCharacters + 1; element++) {
         let image = $('<img>',{id:element, src:'./assets/portraits/'+element+'.png', class:'image'})
         image.click((event) => {handleSelectClick(event, selectedElements)})
-        const cell = $('<td></td>')
-        cell.append(image)
-        portraits.append(cell);           
+        const div = $('<div></div>')
+        div.append(image)
+        div.addClass('rowSpacing')
+        portraits.append(div);           
     }
 
     let playerOneScore = 0;
     let playerTwoScore = 0;
-    const playerOne = $('#playerOneScore')
-    playerOne.append(playerOneScore)
-    const playerTwo = $('#playerTwoScore')
-    playerTwo.append(playerTwoScore)
+    const playerOneElement = $('#playerOneScore')
+    playerOneElement.append(playerOneScore)
+    const playerTwoElement = $('#playerTwoScore')
+    playerTwoElement.append(playerTwoScore)
 
     const playerOneButton = $('#playerOneButton')
     playerOneButton.click(() => {
         playerOneScore++
-        handleScoreClick(playerOne, playerOneScore)
+        const playerOne = 'Player 1'
+        handleScoreClick(playerOneElement, playerOneScore, playerOne)
     })
     const playerTwoButton = $('#playerTwoButton')
     playerTwoButton.click(() => {
         playerTwoScore++
-        handleScoreClick(playerTwo, playerTwoScore)
+        const playerTwo = 'player 2'
+        handleScoreClick(playerTwoElement, playerTwoScore, playerTwo)
     })
     
 });
