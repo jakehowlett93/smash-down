8f6383475c5b8d1a474cd38ad2e99f9998d5ff3d
Jake Howlett <41288146+jakehowlett93@users.noreply.github.com>
2020-04-15 13:54:58 +0100

    refactored two functions in to one

--- index.js
+++ index.js
@@ -24,4 +24,42 @@ $(document).ready(function () {
             element.addClass("selected");
         }
     }
+
+    const handleScoreClick = (playerDiv, playerScore) => {
+        playerDiv.empty()
+        playerDiv.append(playerScore)
+        $('.form').addClass('hideElement')
+    }
+
+
+    let selectedElements = [];
+    
+    const portraits = $('.portraits:first');
+    portraits.addClass('table')
+    for (let element = 1; element < 81; element++) {
+        let image = $('<img>',{id:element, src:'./assets/portraits/'+element+'.png', class:'image'})
+        image.click((event) => {handleSelectClick(event, selectedElements)})
+        const cell = $('<td></td>')
+        cell.append(image)
+        portraits.append(cell);           
+    }
+
+    let playerOneScore = 0;
+    let playerTwoScore = 0;
+    const playerOne = $('#playerOneScore')
+    playerOne.append(playerOneScore)
+    const playerTwo = $('#playerTwoScore')
+    playerTwo.append(playerTwoScore)
+
+    const playerOneButton = $('#playerOneButton')
+    playerOneButton.click(() => {
+        playerOneScore++
+        handleScoreClick(playerOne, playerOneScore)
+    })
+    const playerTwoButton = $('#playerTwoButton')
+    playerTwoButton.click(() => {
+        playerTwoScore++
+        handleScoreClick(playerTwo, playerTwoScore)
+    })
+    
 });
