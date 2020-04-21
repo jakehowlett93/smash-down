90b15007633cf156648578c72015bedb591ea68f
Jake Howlett <41288146+jakehowlett93@users.noreply.github.com>
2020-04-21 16:17:08 +0100

    more tests

--- src/addClickEvent/handleScoreClick/index.js
+++ src/addClickEvent/handleScoreClick/index.js
@@ -3,7 +3,7 @@ import $ from 'jquery';
 const handleScoreClick = (playerElement, player, playerScore, totalCharacters) => {
     playerElement.empty()
     playerElement.append(playerScore)
-    $('.form input').attr('disabled', true)
+    $('.form input').attr('disabled', true).addClass('disabled')
     const scorePlayerWinsAt =  (totalCharacters / 2)
     if (playerScore === scorePlayerWinsAt - 5) {
         alert(player + ' is 5 wins from winning the smash down')
@@ -11,7 +11,7 @@ const handleScoreClick = (playerElement, player, playerScore, totalCharacters) =
     else if (playerScore === scorePlayerWinsAt - 1) {
         alert(player + ' is 1 win from winning the smash down')
     }
-    else if (playerScore > scorePlayerWinsAt) {
+    else if (playerScore >= scorePlayerWinsAt) {
         alert(player + ' has won!')
     }
 }
