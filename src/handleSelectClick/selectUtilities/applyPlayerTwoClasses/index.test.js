e3cde7d76558c954a6574fcd4b534a3a6de5ff78
Jake Howlett <41288146+jakehowlett93@users.noreply.github.com>
2020-04-23 16:44:31 +0100

    more tests

---
+++ src/handleSelectClick/selectUtilities/applyPlayerTwoClasses/index.test.js
@@ -0,0 +1,25 @@
+import $ from 'jquery';
+import applyPlayerTwoClasses from './index';
+
+describe('applyPlayerTwoClasses', () => {
+    document.body.innerHTML = 
+    '<div class="parent">' +
+    '   <div class="portraits playerTwoCursor"><div>'
+    '</div>'
+
+    const portraits = $('.portraits')
+
+    beforeEach(() => {
+        applyPlayerTwoClasses(portraits)
+    });
+
+    it('should add blueTint to the parent element', () => {
+        expect($('.parent').hasClass('blueTint')).toBeTruthy()
+    });
+    it('should remove the playerTwoCursor class from the portraits element', () => {
+        expect(portraits.hasClass('playerTwoCursor')).toBeFalsy()
+    });
+    it('should add the playerOneCursor class to the portraits element', () => {
+        expect(portraits.hasClass('playerOneCursor')).toBeTruthy()
+    });
+});
